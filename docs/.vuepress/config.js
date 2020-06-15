0a0d8b8ac5595d6005bef8505f8cda340c6fbc73
kaki1 <kakigakki@gmail.com>
2020-06-15 19:01:48 +0900

    a

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,24 +1,27 @@
 module.exports = {
-  title: '@vuepress/theme-blog',
-  description: 'Official blog theme for VuePress',
-  themeConfig: {
-    repo: 'vuepressjs/vuepress-theme-blog',
-    docsDir: 'docs',
-    editLinks: true,
-    editLinkText: 'Edit this page on GitHub',
-    nav: [
-      { text: 'Guide', link: '/' },
-      { text: 'Config', link: '/config/' },
-    ],
-    sidebarDepth: 3,
-    sidebar: {
-      '/config/': [
-        '',
-        'front-matter',
-        'palette'
-      ],
+    title: 'KAKi BLOG',
+    description: '学习，记录，分享，提升',
+    themeConfig: {
+        repo: 'vuepressjs/vuepress-theme-blog',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Edit this page on GitHub',
+        nav: [
+            { text: '编程', link: '/code/' },
+            { text: '随笔', link: '/diary/' },
+        ],
+        sidebarDepth: 3,
+        smoothScroll: true,
+        footer: {
+            contact: [{
+                    type: 'github',
+                    link: 'https://github.com/kakigakki',
+                },
+                {
+                    type: 'mail',
+                    link: 'kakigakki@gmail.com',
+                },
+            ],
+        },
     },
-    smoothScroll: true,
-  },
 }
-
