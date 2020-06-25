11b0dc5b1d18dd8e81d630cd8b728e926bc1dc38
kakigakki <kakigakki@yahoo.co.jp>
2020-06-25 20:58:33 +0900

    container plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,38 +3,53 @@ module.exports = {
     title: 'KAKi BLOG',
     description: '学习，记录，分享，提升',
     themeConfig: {
-        repo: 'kakigakki/me-blog',
-        docsDir: 'docs',
-        editLinks: true,
-        editLinkText: 'Edit this page on GitHub',
-        nav: [
-            { text: '编程', link: '/' },
-            {
-                text: '随笔',
-                link: '/writings/'
-            },
-            {
-                text: '标签',
-                link: '/tag/'
-            },
-            {
-                text: 'github',
-                link: 'https://github.com/kakigakki'
-            },
-        ],
-        smoothScroll: true,
-        footer: {
-            contact: [{
-                    type: 'github',
-                    link: 'https://github.com/kakigakki',
-                },
-                {
-                    type: 'mail',
-                    link: 'kakigakki@gmail.com',
-                },
-            ],
+      repo: 'kakigakki/me-blog',
+      docsDir: 'docs',
+      editLinks: true,
+      editLinkText: 'Edit this page on GitHub',
+      nav: [
+        { text: '编程', link: '/' },
+        {
+          text: '随笔',
+          link: '/writings/'
         },
-        lastUpdated: 'Last Updated', // string | boolean
-    }
+        {
+          text: '标签',
+          link: '/tag/'
+        },
+        {
+          text: 'github',
+          link: 'https://github.com/kakigakki'
+        },
+      ],
+      smoothScroll: true,
+      footer: {
+        contact: [{
+            type: 'github',
+            link: 'https://github.com/kakigakki',
+          },
+          {
+            type: 'mail',
+            link: 'kakigakki@gmail.com',
+          },
+        ],
+      },
+      lastUpdated: 'Last Updated', // string | boolean
+    },
+    plugins: [
+        [
+          'vuepress-plugin-container',
+          {
+            type: 'ctnr',
+            before: `<div class="blockquote">`,
+            after: '</div>',
+          },
+        ],
+        ['vuepress-plugin-container', {
+            type: '点击展开',
+            before: info => `<details class="blockquote">${info ? `<summary>${info}</summary>` : ''}\n`,
+            after: () => '</details>\n'
+      }],
+  ],
 
 }
